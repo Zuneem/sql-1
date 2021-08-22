fc6b933cb2702c946d5d8e19b480b8e0be70965b
Zuneem <88955869+Zuneem@users.noreply.github.com>
2021-08-21 19:07:49 -0500

    Update join_tables.sql

--- join_tables.sql
+++ join_tables.sql
@@ -3,3 +3,6 @@ on products.today = canvas_store.today
 
 select * from products right join canvas_store 
 on products.today = canvas_store.today
+
+select * from (select * from products) p full outer join ( select * from canvas_store) c
+on  p.today = c.today
